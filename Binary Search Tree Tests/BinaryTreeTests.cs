bbcd7b0e8209d74f43be3221cbb995ec9c517f54
Marko - laptop <madevcic@gmail.com>
2015-07-14 09:26:53 +0200

    initial

---
+++ Binary Search Tree Tests/BinaryTreeTests.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Linq;
+using MarkoDevcic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BinaryTreeTests
+    {
+        [TestMethod]
+        public void TestSortedValues()
+        {
+            var tree = new BinaryTree<int>();
+
+            var size = 100000;
+            var numbers = new int[size];
+            var rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = rand.Next(0, size);
+                tree.Insert(numbers[i]);
+            }
+
+            Array.Sort(numbers);
+            var sortedTreeValues = tree.GetSortedValues().ToList();
+            for (int i = 0; i < size; i++)
+            {
+                Assert.AreEqual(numbers[i], sortedTreeValues[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestTreeCount()
+        {
+            var tree = new BinaryTree<int>();
+            var size = 100;
+
+            Node<int> node = null;
+            for (int i = 0; i < size; i++)
+            {
+                node = tree.Insert(i);
+            }
+
+            Assert.AreEqual(tree.Count, size);
+
+            tree.Delete(node);
+
+            Assert.AreEqual(tree.Count, size - 1);
+
+            tree.Insert(101);
+
+            Assert.AreEqual(tree.Count, size);
+        }
+
+        [TestMethod]
+        public void TestIsEmpty()
+        {
+            var tree = new BinaryTree<string>();
+            Assert.IsTrue(tree.IsEmpty);
+
+            tree.Insert("a");
+
+            Assert.IsFalse(tree.IsEmpty);
+        }
+
+        [TestMethod]
+        public void TestGetMaximum()
+        {
+            var tree = new BinaryTree<int>();
+
+            try
+            {
+                tree.GetMaximum();
+                Assert.Fail("Should not get here if tree is empty");
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+
+            var size = 100;
+            for (int i = 0; i < size; i++)
+            {
+                tree.Insert(i);
+            }
+
+            Assert.AreEqual(tree.GetMaximum(), size - 1);
+        }
+
+        [TestMethod]
+        public void TestGetMinimum()
+        {
+            var tree = new BinaryTree<int>();
+
+            try
+            {
+                tree.GetMinimum();
+                Assert.Fail("Should not get here if tree is empty");
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+
+            var size = 100;
+            for (int i = 0; i < size; i++)
+            {
+                tree.Insert(i);
+            }
+
+            Assert.AreEqual(tree.GetMinimum(), 0);
+        }
+
+        [TestMethod]
+        public void TestGetValues()
+        {
+            var tree = new BinaryTree<int>();
+
+            var size = 10000;
+            var random = new Random();
+            var numbers = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = random.Next();
+                tree.Insert(numbers[i]);
+            }
+
+            var values = tree.GetValues().ToList();
+
+            Assert.AreEqual(values.Count, numbers.Length);
+
+            foreach (var n in numbers)
+            {
+                Assert.IsTrue(values.Contains(n));
+                values.Remove(n);
+            }
+
+            Assert.AreEqual(values.Count, 0);
+        }
+
+        [TestMethod]
+        public void TestDelete()
+        {
+            var tree = new BinaryTree<int>();
+            var size = 10;
+            for (int i = 0; i < size; i++)
+            {
+                tree.Insert(i);
+            }
+
+            Assert.AreEqual(tree.Count, size);
+
+            Node<int> node;
+            for (int i = 0; i < size; i++)
+            {
+                tree.TryGetNode(i, out node);
+                tree.Delete(node);
+            }
+
+            Assert.AreEqual(tree.Count, 0);
+            Assert.IsTrue(tree.IsEmpty);
+        }
+
+        [TestMethod]
+        public void TestTryGet()
+        {
+            var tree = new BinaryTree<int>();
+            var size = 10;
+            for (int i = 0; i < size; i++)
+            {
+                tree.Insert(i);
+            }
+
+            Node<int> node;
+            for (int i = 0; i < size; i++)
+            {
+                Assert.IsTrue(tree.TryGetNode(i, out node));
+            }
+
+            Assert.IsFalse(tree.TryGetNode(1000, out node));
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            var tree = new BinaryTree<int>();
+            var size = 10;
+            for (int i = 0; i < size; i++)
+            {
+                tree.Insert(i);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Assert.IsTrue(tree.Contains(i));
+            }
+
+            Assert.IsFalse(tree.Contains(1000));
+        }
+
+        [TestMethod]
+        public void TestGetSuccessor()
+        {
+            var tree = new BinaryTree<int>();
+            var size = 10;
+            Node<int> node = null;
+            for (int i = 0; i < size; i++)
+            {
+                node = tree.Insert(i);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                tree.TryGetNode(i, out node);
+                var next = tree.Successor(node);
+                if (i < size - 1)
+                    Assert.AreEqual(node.Value + 1, next.Value);
+                else
+                    Assert.IsNull(next);
+            }
+        }
+
+        [TestMethod]
+        public void TestGetPredecessor()
+        {
+            var tree = new BinaryTree<int>();
+            var size = 10;
+            Node<int> node = null;
+            for (int i = 0; i < size; i++)
+            {
+                node = tree.Insert(i);
+            }
+
+            for (int i = size - 1; i >= 0; i--)
+            {
+                tree.TryGetNode(i, out node);
+                var prev = tree.Predecessor(node);
+                if (i > 0)
+                    Assert.AreEqual(node.Value - 1, prev.Value);
+                else
+                    Assert.IsNull(prev);
+            }
+        }
+    }
+}
---
+++ Binary Search Tree/BinaryTree.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarkoDevcic
+{
+    public class BinaryTree<T> where T : IComparable<T>
+    {
+        private Int32 size;
+
+        private Node<T> root;
+
+        public bool IsEmpty
+        {
+            get { return root == null; }
+        }
+
+        public Int32 Count { get { return size; } }
+
+        public Node<T> Insert(T value)
+        {
+            size++;
+
+            var node = new Node<T>(value);
+            var current = root;
+            var parent = default(Node<T>);
+            while (current != null)
+            {
+                parent = current;
+                if (value.CompareTo(current.Value) < 0)
+                    current = current.Left;
+                else
+                    current = current.Right;
+            }
+            node.Parent = parent;
+            if (parent == null)
+            {
+                root = node;
+                return root;
+            }
+
+            if (value.CompareTo(parent.Value) < 0)
+                parent.Left = node;
+            else
+                parent.Right = node;
+
+            return node;
+        }
+
+
+        public void Delete(Node<T> node)
+        {
+            if (node == null)
+                throw new ArgumentNullException("null");
+
+            var deleteNode = default(Node<T>);
+            var next = default(Node<T>);
+
+            if (node.Left == null || node.Right == null)
+                deleteNode = node;
+            else
+                deleteNode = Successor(node);
+
+            if (deleteNode.Left != null)
+                next = deleteNode.Left;
+            else
+                next = deleteNode.Right;
+
+            if (next != null)
+                next.Parent = deleteNode.Parent;
+
+            if (deleteNode.Parent == null)
+                root = next;
+            else if (deleteNode.Parent.Left == deleteNode)
+                deleteNode.Parent.Left = next;
+            else
+                deleteNode.Parent.Right = next;
+
+            if (deleteNode != node)
+                node.Value = deleteNode.Value;
+
+            size--;
+        }
+
+
+        public IEnumerable<T> GetValues()
+        {
+            var values = new List<T>();
+            PreorderTreeWalk(root, values);
+            return values;
+        }
+
+        private void PreorderTreeWalk(Node<T> current, ICollection<T> output)
+        {
+            if (current != null)
+            {
+                output.Add(current.Value);
+                PreorderTreeWalk(current.Left, output);
+                PreorderTreeWalk(current.Right, output);
+            }
+        }
+
+        public IEnumerable<T> GetSortedValues()
+        {
+            var values = new List<T>();
+            InorderTreeWalk(root, values);
+            return values;
+        }
+
+        private void InorderTreeWalk(Node<T> current, ICollection<T> output)
+        {
+            if (current != null)
+            {
+                InorderTreeWalk(current.Left, output);
+                output.Add(current.Value);
+                InorderTreeWalk(current.Right, output);
+            }
+        }
+
+        private IEnumerable<T> IterativePreorderTreeWalk()
+        {
+            if (root == null)
+                yield break;
+
+            var stack = new Stack<Node<T>>();
+            var current = root;
+            var finished = false;
+            while (finished == false)
+            {
+                if (current != null)
+                {
+                    yield return current.Value;
+                    stack.Push(current);
+                    current = current.Left;
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                        finished = true;
+
+                    current = stack.Pop();
+                    current = current.Right;
+                }
+            }
+        }
+
+        private IEnumerable<T> IterativeInorderTreeWalk()
+        {
+            if (root == null)
+                yield break;
+
+            var stack = new Stack<Node<T>>();
+            var current = root;
+            var finished = false;
+            while (finished == false)
+            {
+                if (current != null)
+                {
+                    stack.Push(current);
+                    current = current.Left;
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                        finished = true;
+                    else
+                    {
+                        current = stack.Pop();
+                        yield return current.Value;
+                        current = current.Right;
+                    }
+                }
+            }
+        }
+
+        public T GetMinimum()
+        {
+            var node = Minimum(root);
+            if (node == null)
+                throw new InvalidOperationException("Tree is empty");
+            return node.Value;
+        }
+
+        private Node<T> Minimum(Node<T> node)
+        {
+            if (node == null)
+                return null;
+
+            var current = node;
+            while (current.Left != null)
+            {
+                current = current.Left;
+            }
+            return current;
+        }
+
+        public T GetMaximum()
+        {
+            var max = Maximum(root);
+            if (max == null)
+                throw new InvalidOperationException("Tree is empty");
+
+            return max.Value;
+        }
+
+        private Node<T> Maximum(Node<T> node)
+        {
+            if (node == null)
+                return null;
+
+            var current = node;
+            while (current.Right != null)
+            {
+                current = current.Right;
+            }
+            return current;
+        }
+
+
+        public bool Contains(T value)
+        {
+            return IterativeTreeSearch(value) != null;
+        }
+
+        public bool TryGetNode(T value, out Node<T> node)
+        {
+            node = TreeSearch(root, value);
+            return node != null;
+        }
+
+        private Node<T> TreeSearch(Node<T> node, T value)
+        {
+            if (node == null)
+                return null;
+
+            if (node.Value.CompareTo(value) == 0)
+                return node;
+
+            if (value.CompareTo(node.Value) < 0)
+                return TreeSearch(node.Left, value);
+            else
+                return TreeSearch(node.Right, value);
+        }
+
+        private Node<T> IterativeTreeSearch(T value)
+        {
+            var current = root;
+            while (current != null)
+            {
+                if (current.Value.CompareTo(value) == 0)
+                    break;
+
+                if (value.CompareTo(current.Value) < 0)
+                    current = current.Left;
+                else
+                    current = current.Right;
+            }
+
+            return current;
+        }
+
+        public Node<T> Successor(Node<T> node)
+        {
+            if (node == null)
+                throw new ArgumentNullException("node");
+
+            if (node.Right != null)
+                return Minimum(node.Right);
+
+            var parent = node.Parent;
+            var rightChild = node;
+
+            while (parent != null && rightChild == parent.Right)
+            {
+                rightChild = parent;
+                parent = parent.Parent;
+            }
+
+            return parent;
+        }
+
+        public Node<T> Predecessor(Node<T> node)
+        {
+            if (root == null)
+                throw new ArgumentNullException("node");
+
+            if (node.Left != null)
+                return Maximum(node.Left);
+
+            var parent = node.Parent;
+            var leftChild = node;
+
+            while (parent != null && parent.Left == leftChild)
+            {
+                leftChild = parent;
+                parent = parent.Parent;
+            }
+
+            return parent;
+        }
+    }
+
+    public sealed class Node<TValue> : IComparable<Node<TValue>> where TValue : IComparable<TValue>
+    {
+        public Node<TValue> Left { get; internal set; }
+
+        public Node<TValue> Right { get; internal set; }
+
+        public Node<TValue> Parent { get; internal set; }
+
+        public TValue Value { get; internal set; }
+
+        internal Node(TValue value)
+        {
+            Value = value;
+        }
+
+        internal Node(Node<TValue> parent, Node<TValue> left, Node<TValue> right, TValue value)
+        {
+            Parent = parent;
+            Left = left;
+            Right = right;
+            Value = value;
+        }
+
+        public int CompareTo(Node<TValue> other)
+        {
+            if (other == null)
+            {
+                return -1;
+            }
+
+            return this.Value.CompareTo(other.Value);
+        }
+    }
+}
